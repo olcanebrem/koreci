33659c4f0e004b5bddf2184bd35fd6ad56e141df
Olcan EBREM <olcanebrem@gmail.com>
2025-03-09 05:10:13 +0300

    netlify-functions

---
+++ netlify/functions/getMapData.js
@@ -0,0 +1,45 @@
+// netlify/functions/getMapData.js
+exports.handler = async (event, context) => {
+    const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+    if (!apiKey) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: "API key is not defined" }),
+      };
+    }
+  
+    const config = {
+      locations: [
+        {
+          title: "Sarıyer",
+          address1: "Sarıyer/İstanbul",
+          address2: "Türkiye",
+          coords: { lat: 41.17817959025185, lng: 29.041851081506355 },
+          placeId: "ChIJ11KoMiLgn0ARxE4ae9DKhjo",
+        },
+      ],
+      mapOptions: {
+        center: { lat: 41.17817959025185, lng: 29.041851081506355 },
+        fullscreenControl: true,
+        mapTypeControl: false,
+        streetViewControl: false,
+        zoom: 12,
+        zoomControl: true,
+        maxZoom: 17,
+        mapId: "",
+      },
+      capabilities: {
+        input: false,
+        autocomplete: false,
+        directions: false,
+        distanceMatrix: false,
+        details: false,
+        actions: false,
+      },
+    };
+  
+    return {
+      statusCode: 200,
+      body: JSON.stringify(config),
+    };
+  };
